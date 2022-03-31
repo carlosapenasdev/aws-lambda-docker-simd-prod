452164320973ccafe6ae222c590bc052bcf762fb
Carlos Eduardo Almeida Santos <carlos.santos.wd@gmail.com>
2022-03-31 17:39:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# AWS Lambda Docker Python Pillow-SIMD / Teste Deploy Fora
+
+Registrar conteúdo de aprendizagem sobre AWS Lambda, utilizando Docker para um runtime python instalando
+a lib Pillow-SIMD.
+
+A lib Pillow-SIMD informa ser mais veloz que a lib original (Pillow) por utilizar
+libs compiladas para o SO específico.
+
+O uso do docker para gerenciar a função lambda, foi escolhido por facilitar o processo de instalação e manutenção
+das dependencias do projeto.
+
+O objetivo final é comparar a performance com diferentes libs de resize e imagens, tendo um mesmo ambiente base para as funções lambda.
+
+## 0. Requisitos
+- AWS Cli ([link sobre instalação](https://docs.aws.amazon.com/pt_br/cli/latest/userguide/getting-started-install.html))
+- AWS Sam ([link sobre instalação](https://docs.aws.amazon.com/pt_br/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html))
+-  clonar este repositorio localmente:
+```bash
+git clone git@github.com:carlossantoswd/aws-lambda-docker-reven.git .
+```
+## 1. Testar a função lambda local
+```bash
+sam build && sam local invoke DockerRevenFunction
+```
+## 2. Caso faça alterações no app.py após ter dado build:
+```bash
+docker rmi $(docker image ls dockerrevenfunction -q) && sam build && sam local invoke DockerRevenFunction
+```
+
+## 3. Deploy para amazon
+```bash
+sam deploy --stack-name docker-reven  --resolve-s3 --resolve-image-repos --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM
+```
---
+++ src/app.py
@@ -0,0 +1,63 @@
+import boto3
+import os
+import sys
+import uuid
+from PIL import Image
+import PIL.Image
+import re
+
+regex = r"(_\w_)"
+
+s3_client = boto3.client('s3')
+
+class Size:
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+    
+def localImg(bktOri, keyFile):
+    localFile           = '/tmp/{}'.format(keyFile)
+    s3_client.download_file(bktOri, keyFile, localFile)
+    return localFile
+
+def thumbLocal(localFile, keyFile, size):
+    dimension = getSize(size)
+    local_resized = resize_local(localFile, size, dimension.width, dimension.height)
+    s3_client.upload_file(local_resized, 'bucket-store-blossv1', re.sub(regex, '_'+size+'_', keyFile, 0, re.MULTILINE))
+    return local_resized
+
+def getSize(size):
+    return {
+        'W': Size(1920 ,1080),
+        'B': Size(800 ,600),
+        'G': Size(640 ,480),
+        'M': Size(485 ,363),
+        'S': Size(260 ,195),
+        'V': Size(172 ,130),
+        'P': Size(72 ,54),
+    }[size]
+
+def resize_local(localFile, size, width, height):
+    local_resized = localFile.replace('_O_', '_'+size+'_')
+    with Image.open(localFile) as image:
+         image.thumbnail((width, height),PIL.Image.ANTIALIAS)
+         image.save(local_resized)
+    return local_resized
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        localFile = localImg(bucket, key)
+        
+        s3_client.upload_file(localFile, 'bucket-store-blossv1', key)
+        s3_client.delete_object(Bucket=bucket, Key=key)
+
+        localFile = thumbLocal(localFile, key, 'W')
+        localFile = thumbLocal(localFile, key, 'B')
+        localFile = thumbLocal(localFile, key, 'G')
+        localFile = thumbLocal(localFile, key, 'M')
+        localFile = thumbLocal(localFile, key, 'S')
+        localFile = thumbLocal(localFile, key, 'V')
+        localFile = thumbLocal(localFile, key, 'P')
